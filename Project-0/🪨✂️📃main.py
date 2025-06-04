36e7fc632bea546292d3095af3755ccfe07ea13d
Param-Patel-o5 <parampatel.005@gmail.com>
2025-06-04 17:36:52 +0530

    Update 🪨✂️📃main.py

--- Project-0/🪨✂️📃main.py
+++ Project-0/🪨✂️📃main.py
@@ -2,40 +2,45 @@
 
 import random
 
+
 computer = random.choice([0, -1, 1])
 
+youstr=None
+gesture = ["rock" ,"paper", "scissors" ]
 
-youstr=input("Enter your gesture:")
-youdict={"rock":1,"paper":0,"scissors":-1}
+while youstr not in gesture :
+    youstr=input("Enter your gesture:").lower()
+    youdict={"rock":1,"paper":0,"scissors":-1}
+
 you= youdict[youstr]
 
 # Reverse dictionary to get name from value
 reversed_dict = {v: k for k, v in youdict.items()}
 
 if(computer == -1 and you ==1):
- print("you win")
+    print("you win")
 
  
 elif(computer == -1 and you ==0):
- print("you loose")
+    print("you loose")
 
 
 if(computer == 0 and you ==1):
- print("you loose")
+    print("you loose")
 
  
 elif(computer == 0 and you ==-1):
- print("you win")
+    print("you win")
 
 
 if(computer == 1 and you ==-1):
- print("you loose")
+    print("you loose")
 
  
 elif(computer == 1 and you ==0):
- print("you win")
+    print("you win")
 
 if(computer == you):
- print("It's a draw.")
+    print("It's a draw.")
 
 print(f"Computer gesture was: {reversed_dict[computer]}")
